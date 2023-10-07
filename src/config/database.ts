9d1caf4eaf6b68e612e7eec12db2cd1a8c56c226
AlbertHuang01 <37929454+AlbertHuang01@users.noreply.github.com>
2023-10-08 01:35:06 +0800

    添加 vercel 配置，修改数据库配置

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,11 +2,11 @@ import {join} from 'path';
 
 export default {
     type: 'mysql',
-    host: 'localhost',
-    port: 49153,
-    username: 'root',
+    host: 'db4free.net',
+    port: 3306,
+    username: 'vsdfdsf',
     password: 'wFDfaWI8VSBWu5pH',
-    database: 'ai-server',
+    database: 'ai_server',
     entities: [join(__dirname, '../', '**/**.entity{.ts,.js}')],
     synchronize: true,
 };
